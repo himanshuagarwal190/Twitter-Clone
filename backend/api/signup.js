3ae58afea695ff8b345464de5c9b67b5f2337bce
Himanshu Agarwal <himanshuagarwal190@gmail.com>
2021-01-12 19:15:41 +0530

    handle update

--- backend/api/signup.js
+++ backend/api/signup.js
@@ -4,15 +4,30 @@ const sha256 = require('sha256')
 module.exports = (app) =>{
     // create user
     app.post('/api/signup', async (req, res) =>{
+        let handle
+        if(req.body.handle.includes('@')){
+            handle = req.body.handle
+        }
+        else{
+            handle = '@' + req.body.handle
+        }
+
         const user = {
             email: req.body.email,
             password: sha256(req.body.password),
-            handle: '@' + req.body.handle
+            handle
         }
 
-        const emailExist = await User.exists({email: req.body.email})
+        const emailExist = await User.exists({email: user.email})
+        const handleExist = await User.exists({handle: user.handle})
 
-        if(!emailExist){
+        if(emailExist){
+            res.send({status: 401, message: 'Email already exists'})
+        }
+        else if(handleExist){
+            res.send({status: 401, message: 'Handle already exists'})
+        }
+        else{
             User.create(user, (err, done) =>{
                 if(err){
                     console.log('Error')
@@ -21,9 +36,6 @@ module.exports = (app) =>{
                     res.send({status: 200, message: 'User created'})
                 }
             })
-        }
-        else{
-            res.send({status: 401, message: 'Email already exists'})
         }
     })
 }
